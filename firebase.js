9e4a956cc39f9323d8e1f5cc0ffd056c1d0a68bd
JaredClifford <61440622+JaredClifford@users.noreply.github.com>
2020-11-12 01:06:14 -0600

    Friends
    Lets you search for people and add them as a friend :)

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 var db = firebase.firestore();
+// Your web app's Firebase configuration
+
 
 function handleSignUp() {
     var firstName = document.getElementById("inputFName").value;
@@ -16,20 +18,19 @@ function handleSignUp() {
     // Create user with email and pass.
     // [START createwithemail]
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                db.collection("users").doc(user.uid).set({
-                email: email,
-                firstName: firstName,
-                lastName: lastName
-                }).then(function() {
-                    console.log("Document successfully written!");
-                    document.getElementById('sign-up-form').style.display='none';
-                }).catch(function(error) {
-                    console.log("error adding document: ", error);
-                })
-            }
-        });
+        if (user) {
+            db.collection("users").doc(user.uid).set({
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            friends: {}
+            }).then(function() {
+                console.log("Document successfully written!");
+                document.getElementById('sign-up-form').style.display='none';
+            }).catch(function(error) {
+                console.log("error adding document: ", error);
+            })
+        }
     }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
@@ -66,19 +67,17 @@ function signIn() {
     var email = document.getElementById("inputEmail2").value;
     var password = document.getElementById("inputPassword2").value;
     firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                db.collection("users").get().then((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                        if (doc.id == user.uid) {
-                            document.getElementById('sign-in-form').style.display='none';
-                            alert('Welcome!');
-                            // console.log(doc.data().firstName);
-                        } 
-                    });
+        if (user) {
+            db.collection("users").get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if (doc.id == user.uid) {
+                        document.getElementById('sign-in-form').style.display='none';
+                        alert('Welcome!');
+                        // console.log(doc.data().firstName);
+                    } 
                 });
-            }
-        });
+            });
+        }
       }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -98,4 +97,84 @@ function signout() {
         console.error("Log out not successful", error)
       });
       
+}
+
+function searchPeople() {
+    var value = document.getElementById("friendSearch").value;
+    var list = inputToTuple(value);
+    people = new Array;
+    var count = 0;
+    if (list[1] != null) {
+        db.collection("users").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                if (doc.data().firstName.toLowerCase() == list[0].toLowerCase() && doc.data().lastName.toLowerCase() == list[1].toLowerCase()) {
+                    people.push(doc.data().firstName);
+                    console.log(doc.data().firstName, doc.data().lastName);
+                    count = 1;
+                }
+            });
+        });
+    } else {
+        db.collection("users").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                if (doc.data().firstName.toLowerCase() == list[0].toLowerCase() || doc.data().lastName.toLowerCase() == list[0].toLowerCase()) {
+                    people.push(doc);
+                    console.log(doc.data().firstName, doc.data().lastName);
+                    count = 1;
+                }
+            });
+            querySnapshot.forEach((doc) => {
+                if (doc.data().firstName.toLowerCase().search(list[0].toLowerCase()) != -1 || doc.data().lastName.toLowerCase().search(list[0].toLowerCase()) != -1) {
+                    if (!people.includes(doc)) {
+                        people.push(doc);
+                        console.log(doc.data().firstName, doc.data().lastName);
+                        count = 1;
+                    }
+                }
+            });
+            return people;
+        }).then(listToInnerText);
+    }
+    
+}
+
+function listToInnerText(docs) {
+    docs.forEach((doc) => {
+        var div = document.createElement('div');
+        div.setAttribute(`onclick`,`addFriend("${ doc.id }")`);
+        div.innerHTML = `${doc.data().firstName}`;
+        document.getElementById('list').appendChild(div);
+    })
+}
+
+function addFriend(doc) {
+    var user = firebase.auth().currentUser;
+    if (user) {
+        console.log(user.uid);
+        console.log(doc)
+        db.collection("users").doc(user.uid).collection("friends").doc(doc).set({
+        }).then(function() {
+            console.log("Document successfully written!");
+        });
+        parent = document.getElementById('list');
+        while (parent.lastChild) {
+            parent.removeChild(parent.lastChild);
+        }
+    } else {
+        console.log("Not currently signed in");
+    }
+
+}
+
+function inputToTuple(input) {
+    var tuple = ["","s"];
+    if (input.lastIndexOf(" ") != input.indexOf(" ")) {
+        console.error("incorrect syntax for name");
+        return null;
+    }
+    if (input.indexOf(" ") == -1)
+        return [input,null];
+    tuple[0] = input.substring(0,input.indexOf(" "));
+    tuple[1] = input.substring(input.indexOf(" ")+1, input.length);
+    return tuple;
 }
