427849ddbadad4032597f937027833fa16c81b8b
JaredClifford <61440622+JaredClifford@users.noreply.github.com>
2020-11-22 20:16:17 -0600

    add music

--- myProfile.js
+++ myProfile.js
@@ -165,7 +165,7 @@ function getMovies(){
                                     <p class="text-muted">${movie_maker}</p>
                                 </div>
                                 <div class="thumb-lg member-thumb mx-auto mb-2"><img src="${poster_image}" class=" img-thumbnail" alt="profile-image"></div>
-                                <button id="like-${data2.id}" class="like" onclick="likeMovieClick('${data2.id}',0)">
+                                <button id="like-${data2.id}" class="like" onclick="likeMovieClick('${data2.id}')">
                                     <i class="fa fa-thumbs-o-up fa-2x" aria-hidden="true"></i>
                                 </button>
                                 <button id="dislike-${data2.id}" class="dislike" onclick="dislikeMovieClick('${data2.id}')">
@@ -191,7 +191,7 @@ function getMovies(){
                                 </input>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-primary" onclick="saveMovieChanges(${data2.id})">Save changes</button>
                                 </div>
                             </div>
                         </div>
@@ -270,7 +270,7 @@ function getShows(){
                                 </input>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-primary" onclick="saveShowChanges(${data2.id})">Save changes</button>
                                 </div>
                             </div>
                         </div>
@@ -280,9 +280,9 @@ function getShows(){
                     showDiv.insertAdjacentHTML('beforeend',html);
                     }).then(function() {
                         if (show.data().rating == 1) {
-                            likeClick(show.id);
+                            document.getElementById(`like-${show.id}`).style.color = "#4278f5";
                         } else if (show.data().rating == -1) {
-                            dislikeClick(show.id);
+                            document.getElementById(`dislike-${show.id}`).style.color = "#f55142";
                         }
                         document.getElementById(`review-show-${show.id}`).value = show.data().review;
                     })
@@ -291,10 +291,107 @@ function getShows(){
         });
 }
 
+/*
+function getMusic() {
+    user = firebase.auth().currentUser;
+    showsDiv = document.getElementById("profile-shows-list");
+    showsDiv.innerHTML = "";
+    var docRef = db.collection("users").doc(user.uid).collection("ShowsList");
+        docRef.get().then((querySnapshot) => {
+            querySnapshot.forEach((music) => {
+                if (music.id != "abcdefghij") {
+                    let url = baseURL + "tv/" + music.id + "?api_key=" + API_key;    //search API Here
+                    fetch(url).then(result => result.json()).then((data2) => {
+                        artist = ""  //Artist Goes here
+                        
+                        poster_image = "https://image.tmdb.org/t/p/w500" + data2.poster_path; //Song image Goes here
+        
+                        const html =
+                    `
+                    <div class="col-lg-4 mt-2">
+                        <div class="text-center card-box">
+                            <div class="member-card pb-2">
+                                <div class="col-12">
+                                    <h4>${data2.name}</h4>
+                                    <p class="text-muted">${artist}</p>
+                                </div>
+                                <div class="thumb-lg member-thumb mx-auto mb-2"><img src="${poster_image}" class=" img-thumbnail" alt="profile-image"></div>
+                                <button id="like-${data2.id}" class="like" onclick="likeMusicClick('${data2.id}')">
+                                    <i class="fa fa-thumbs-o-up fa-2x" aria-hidden="true"></i>
+                                </button>
+                                <button id="dislike-${data2.id}" class="dislike" onclick="dislikeMusicClick('${data2.id}')">
+                                    <i class="fa fa-thumbs-o-down fa-2x" aria-hidden="true"></i>
+                                </button>
+                                <div>
+                                    <button type="button" id="" class="ml-2 btn btn-primary mt-2 btn-rounded waves-effect w-md waves-light" data-toggle="modal" data-target="#modal-show-${data2.id}" >Edit Review</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" id="modal-music-${data2.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true" style="color: black;">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Edit ${data2.name} reviews</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <input type="text" class="modal-body" id="review-music-${data2.id}">
+                                </input>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary" onclick="saveMusicChanges(${data2.id})">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                    musicDiv = document.getElementById("profile-music-list");
+                    musicDiv.insertAdjacentHTML('beforeend',html);
+                    }).then(function() {
+                        if (music.data().rating == 1) {
+                            document.getElementById(`like-${music.id}`).style.color = "#4278f5";
+                        } else if (music.data().rating == -1) {
+                            document.getElementById(`dislike-${music.id}`).style.color = "#f55142";
+                        }
+                        document.getElementById(`review-music-${music.id}`).value = music.data().review;
+                    })
+                }
+            });
+        });
+}*/
 
 
 
+function saveMovieChanges(id) {
+    user = firebase.auth().currentUser;
+    var updated = document.getElementById(`review-movie-${id}`).value;
+    console.log(id);
+    docRef = db.collection("users").doc(user.uid).collection(`MovieList`).doc(`${id}`);
+    console.log(updated);
+    docRef.get().then(function(doc){
+        rating = doc.data().rating;
+        db.collection("users").doc(user.uid).collection(`MovieList`).doc(`${id}`).set({
+            rating: rating,
+            review: updated
+        });
+    });
+}
 
+function saveShowChanges(id) {
+    user = firebase.auth().currentUser;
+    var updated = document.getElementById(`review-show-${id}`).value;
+    docRef = db.collection("users").doc(user.uid).collection(`MovieList`).doc(`${id}`);
+    docRef.get().then(function(doc){
+        rating = doc.data().rating;
+        db.collection("users").doc(user.uid).collection(`MovieList`).doc(`${id}`).set({
+            rating: rating,
+            review: updated
+        });
+    });
+}
 
 function showMovies(){
     getMovies();
